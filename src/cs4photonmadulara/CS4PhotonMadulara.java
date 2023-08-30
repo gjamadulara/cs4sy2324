a08ce7033b10dc5a071bb6f523bc04acdd38b02f
PHOTON <PHOTON@172.22.16.91>
2023-08-30 16:03:53 +0800

    first java project

---
+++ src/cs4photonmadulara/CS4PhotonMadulara.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4photonmadulara;
+
+/**
+ *
+ * @author PHOTON
+ */
+public class CS4PhotonMadulara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Hello madlang pipol mabuhay!";
+        System.out.println( greeting + " Mini Miss U! Mini Miss U!");
+    }
+    
+}
